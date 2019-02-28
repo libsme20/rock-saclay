05c5cded6816d73c3cf20b046dc9f4636895795f
Florent Yaicene <florent.yaicene@protonmail.com>
2019-02-28 16:29:52 +0100

    Adding verify signature in clients

--- client/card_provider_management.py
+++ client/card_provider_management.py
@@ -5,22 +5,15 @@ import struct
 
 from ecdsa import SigningKey, NIST384p
 
-
 from util import debug, h2a, a2h
 
 alphabet = string.ascii_letters
 
 #private_key = SigningKey.generate(curve=NIST384p)
-#string = private_key.to_string();
-
 private_key_string = b'-----BEGIN EC PRIVATE KEY-----\nMIGkAgEBBDAfR/wtBhgTA36/CciBuwBZ4JvdbsJfgQoUiktFiApgslqe9JAqE49c\nD3Oxs1HxMdygBwYFK4EEACKhZANiAATfw2ME1Gf89yLbJVFrcIuctS4jYkL9t6HX\nL1MpXlganW/VB8GdkNI0e7mtB/LqGZpN6v4PKh6VqnJJSc8ON+NTZta3/rP/JkPq\n48pSv8CAPHPuhWq8X44fogzoInh33YU=\n-----END EC PRIVATE KEY-----\n'  
 private_key= SigningKey.from_pem(private_key_string)
 public_key = private_key.get_verifying_key()
 print(public_key.to_pem())
-
-signature = private_key.sign("INFO".encode())
-
-assert public_key.verify(signature, "INFO".encode())
 
 # INSTALL THE APPLET HELPER
 if len(sys.argv) == 2  and sys.argv[1] == "install":
@@ -70,7 +63,8 @@ if len(sys.argv) == 2  and sys.argv[1] == "install":
     pin = struct.pack("!H", pin)
     name_length = struct.pack("B", len(name))
     name = name.encode()
-    args = id+pin+name_length+name
+    signature = private_key.sign(id+name)
+    args = id+pin+name_length+name+signature
     debug("param array", args)
     args = a2h(args)
     debug("param hex", args)
